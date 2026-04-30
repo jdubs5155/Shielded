37a1ae246559301a1e73ffde768a1bbb0ff1bd98
jdubs5155 <jonathan.webb511@gmail.com>
2026-04-30 15:04:37 -0400

    Update Color.kt

--- app/src/main/java/com/aggregatorx/app/ui/theme/Color.kt
+++ app/src/main/java/com/aggregatorx/app/ui/theme/Color.kt
@@ -2,14 +2,44 @@ package com.aggregatorx.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-val DarkBackground = Color(0xFF1C1B1F)
-val DarkSurface = Color(0xFF252429)
+// --- Dark Theme Palette ---
+val PrimaryDark = Color(0xFFD0BCFF)
+val OnPrimaryDark = Color(0xFF381E72)
+val PrimaryContainerDark = Color(0xFF4F378B)
+val OnPrimaryContainerDark = Color(0xFFEADDFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-val LightBackground = Color(0xFFFFFBFE)
-val LightSurface = Color(0xFFF3EDF7)
+val SecondaryDark = Color(0xFFCCC2DC)
+val OnSecondaryDark = Color(0xFF332D41)
+
+val TertiaryDark = Color(0xFFEFB8C8)
+val OnTertiaryDark = Color(0xFF492532)
+
+val BackgroundDark = Color(0xFF1C1B1F)
+val OnBackgroundDark = Color(0xFFE6E1E5)
+val SurfaceDark = Color(0xFF252429)
+val OnSurfaceDark = Color(0xFFE6E1E5)
+val SurfaceVariantDark = Color(0xFF49454F)
+
+// --- Light Theme Palette ---
+val PrimaryLight = Color(0xFF6750A4)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFFEADDFF)
+val OnPrimaryContainerLight = Color(0xFF21005D)
+
+val SecondaryLight = Color(0xFF625B71)
+val OnSecondaryLight = Color(0xFFFFFFFF)
+
+val TertiaryLight = Color(0xFF7D5260)
+val OnTertiaryLight = Color(0xFFFFFFFF)
+
+val BackgroundLight = Color(0xFFFFFBFE)
+val OnBackgroundLight = Color(0xFF1C1B1F)
+val SurfaceLight = Color(0xFFF3EDF7)
+val OnSurfaceLight = Color(0xFF1C1B1F)
+val SurfaceVariantLight = Color(0xFFE7E0EC)
+
+// --- Status & Functional Colors ---
+val Error = Color(0xFFF2B8B5)
+val OnError = Color(0xFF601410)
+val Success = Color(0xFFB5F2B8) // For successful site analysis
+val Warning = Color(0xFFF2E7B5) // For security warnings
