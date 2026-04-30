405123dc2ce73b5e7aa795d933e5150a18636bd1
jdubs5155 <jonathan.webb511@gmail.com>
2026-04-30 05:55:01 -0400

    Update AggregatorDatabase.kt

--- app/src/main/java/com/aggregatorx/app/data/database/AggregatorDatabase.kt
+++ app/src/main/java/com/aggregatorx/app/data/database/AggregatorDatabase.kt
@@ -2,33 +2,20 @@ package com.aggregatorx.app.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.aggregatorx.app.data.model.LearnedUserProfile
-import com.aggregatorx.app.data.model.LikedResult
-import com.aggregatorx.app.data.model.Provider
-import com.aggregatorx.app.data.model.ScrapingConfig
-import com.aggregatorx.app.data.model.SearchHistoryEntry
-import com.aggregatorx.app.data.model.SiteAnalysis
-import com.aggregatorx.app.data.model.UserPreferences
+import com.aggregatorx.app.data.model.ProviderEntity
+import com.aggregatorx.app.data.model.ResultItem
+import com.aggregatorx.app.data.model.AuditLogEntity
 
 @Database(
     entities = [
-        Provider::class,
-        SiteAnalysis::class,
-        ScrapingConfig::class,
-        SearchHistoryEntry::class,
-        UserPreferences::class,
-        LikedResult::class,
-        LearnedUserProfile::class
-    ],
-    version = 3,
+        ProviderEntity::class, 
+        ResultItem::class, 
+        AuditLogEntity::class
+    ], 
+    version = 2, 
     exportSchema = false
 )
 abstract class AggregatorDatabase : RoomDatabase() {
-    abstract fun providerDao(): ProviderDao
-    abstract fun siteAnalysisDao(): SiteAnalysisDao
-    abstract fun scrapingConfigDao(): ScrapingConfigDao
-    abstract fun searchHistoryDao(): SearchHistoryDao
-    abstract fun userPreferencesDao(): UserPreferencesDao
-    abstract fun likedResultDao(): LikedResultDao
-    abstract fun learnedProfileDao(): LearnedProfileDao
+    abstract fun aggregatorDao(): AggregatorDao
+    abstract fun auditLogDao(): AuditLogDao
 }
